221213762ab22afb1c8afcd9075c8ffd20bd06e5
Elbtalkessel <rtfsc@pm.me>
2024-04-30 15:00:50 +0200

    Add greeter

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,6 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  session = "${pkgs.hyprland}/bin/Hyprland";
+  username = "risus";
+in
 {
   imports = [ <home-manager/nixos> ];
+
+  # Gretter with autologin
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "${session}";
+        user = "${username}";
+      };
+      default_session = {
+        command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time --cmd ${session}";
+        user = "greeter";
+      };
+    };
+  };
 
   programs.hyprland = {
     enable = true;
