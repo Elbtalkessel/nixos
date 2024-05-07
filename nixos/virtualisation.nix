1cf5d923d6d3e70fe9b6b3b50d8931b97f40e683
Elbtalkessel <rtfsc@pm.me>
2024-05-07 12:34:17 +0200

    Add qemu

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   imports = [ <home-manager/nixos> ];
-
+ 
+  # DOCKER / PODMAN
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
@@ -17,4 +18,26 @@
   home-manager.users.risus.home.shellAliases = {
     d = "DOCKER_HOST=unix:///run/user/1000/podman/podman.sock lazydocker";
   };
+
+  # QEMU
+  virtualisation.libvirtd = {
+    enable = true;
+  };
+  programs.virt-manager = {
+    enable = true;
+  };
+
+  # virt-manager requires dconf to remember settings
+  programs.dconf = {
+    enable = true;
+  };
+  home-manager.users.risus.dconf = {
+    settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
+  };
+  users.users.risus.extraGroups = [ "libvirtd" ];
 }
