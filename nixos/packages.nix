990005236658b6993d4b506708701592817c6277
Elbtalkessel <rtfsc@pm.me>
2024-05-08 21:07:46 +0200

    sensors

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -3,7 +3,9 @@
 {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [];
+  environment.systemPackages = with pkgs; [
+    lm_sensors
+  ];
   fonts.packages = with pkgs; [
     (nerdfonts.override{ fonts = ["JetBrainsMono"]; })
   ];
