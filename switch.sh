fb7deb1b0d33f629bcaa96ea521d73e73ff223cd
Elbtalkessel <rtfsc@pm.me>
2024-05-11 16:47:46 +0200

    Sat 11 May 2024 16:47:46 CEST

--- switch.sh
+++ switch.sh
@@ -6,11 +6,11 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--home)
       shift
-      HOME=true
+      SWITCH_HOME=true
       ;;
     -s|--system)
       shift
-      SYSTEM=true
+      SWITCH_SYSTEM=true
       ;;
     *)
       echo "CLI for system setup. Passes arguments to other scripts. Each script supports -h, --help for more information, for example ./cli.sh -i -h to see install script help."
@@ -23,13 +23,13 @@ done
 
 
 
-if [ ${HOME} ]; then
+if [ ${SWITCH_HOME} ]; then
   # Calls `home-manager switch` and `hyprctl reload`.
   # For some reason Hyprland doesn't autoreload on configuration change.
   home-manager switch --flake ./
   hyprctl reload
 fi
 
-if [ ${SYSTEM} ]; then
+if [ ${SWITCH_SYSTEM} ]; then
   sudo nixos-rebuild switch --flake ./
 fi
