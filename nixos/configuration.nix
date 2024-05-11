6d1d5d1e90001e9fac31b09d5be036e049c807ea
Elbtalkessel <rtfsc@pm.me>
2024-05-11 11:52:41 +0200

    Remove home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,8 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ ... }:
-
+{ pkgs, ... }:
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  session = "${pkgs.hyprland}/bin/Hyprland";
+  username = "risus";
+in
 {
   imports =
     [
@@ -11,37 +15,46 @@
       # amp cpu + amd cpu pstate + amd gpu + nvidia + ssd
       <nixos-hardware/omen/15-en0010ca>
       ./hardware-configuration.nix
-      ./machine.nix
-      ./users.nix
-      ./packages.nix
-      ./home/home.nix
-      ./home/hyprland.nix
-      ./home/waybar.nix
-      ./home/virtualisation.nix
-      ./home/sound.nix
-      ./home/alacritty.nix
-      ./home/aliases.nix
-      ./home/environment.nix
-      ./home/fish.nix
-      ./home/git.nix
-      ./home/mako.nix
-      ./home/sound.nix
     ];
-  
-  programs.fish.enable = true;
+
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-uuid/6eb60a50-cb6b-48c3-82da-a3ef3aee9a02";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.kernel.sysctl = {
+    # Allow starting server @ :80
+    "net.ipv4.ip_unprivileged_port_start" = 80;
+  };
+
+
   nix.extraOptions = "
     experimental-features = flakes nix-command
   ";
 
+
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.hostName = "omen";
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_IE.UTF-8";
     LC_IDENTIFICATION = "en_IE.UTF-8";
@@ -54,9 +67,13 @@
     LC_TIME = "en_IE.UTF-8";
   };
 
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = (_: true);
 
+
+  # PROGRAMS
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -64,18 +81,99 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  # virt-manager requires dconf to remember settings
+  programs.dconf.enable = true;
+  programs.virt-manager.enable = true;
+  programs.hyprland = {
+    enable = true;
+    xwayland = {
+      enable = true;
+    };
+  };
+  programs.fish.enable = true;
+
+
+  # VIRTUALISATION
+  # DOCKER / PODMAN
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket = {
+      enable = true;
+    };
+  };
+  # QEMU
+  virtualisation.libvirtd.enable = true;
+
+
+  # USERS
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${username} = {
+    isNormalUser = true;
+    description = "${username}";
+    extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "tss" "libvirtd" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINC/PJ5Ll6Z8N0UM+nkMMBCrpf23J963UdeIWZrZjZBg me@0x00.life"
+    ];
+    shell = pkgs.fish;
+  };
+
+
+  # HARDWARE
+  # Bluetooth
+  # doesn't work
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+
+  # SECURITY
+  security.rtkit.enable = true;
+
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
   services.udisks2.enable = true;
+  # Change runtime directory size
+  services.logind.extraConfig = "RuntimeDirectorySize=8G";
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  # Gretter with autologin
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "${session}";
+        user = "${username}";
+      };
+      default_session = {
+        command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time --cmd ${session}";
+        user = "greeter";
+      };
+    };
+  };
+  services.gnome.gnome-keyring.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    lm_sensors
+    home-manager
+  ];
+
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override{ fonts = ["JetBrainsMono"]; })
+  ];
+
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -84,5 +182,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
