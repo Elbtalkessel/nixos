9f4809d07d9b4bd04ca01fe9c27f5fa77c5768bc
Elbtalkessel <rtfsc@pm.me>
2024-05-13 16:31:40 +0200

    Mon 13 May 2024 16:31:40 CEST

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }:
+  outputs = { nixpkgs, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -22,6 +23,7 @@
     # NixOS configuration per host
     nixosConfigurations.omen = nixpkgs.lib.nixosSystem {
       inherit system;
+      specialArgs = { inherit inputs; };
       modules = [
         # The closest one to my laptop,
         # amp cpu + amd cpu pstate + amd gpu + nvidia + ssd
--- system/configuration.nix
+++ system/configuration.nix
@@ -31,6 +31,10 @@ in
   nix.settings.experimental-features = "nix-command flakes";
   # devenv requirement, allows devenv to manager caches.
   nix.settings.trusted-users = [ "root" "risus" ];
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
 
   # Enable networking
@@ -83,6 +87,7 @@ in
     xwayland = {
       enable = true;
     };
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
   programs.fish.enable = true;
 
