90a9617be2587f28f90d2d6da334d6af0801b7c8
Elbtalkessel <rtfsc@pm.me>
2024-05-23 12:00:22 +0200

    Thu 23 May 2024 12:00:22 CEST

--- system/modules/logiops.nix
+++ system/modules/logiops.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }: {
   # Logitech MX Master 3 configuration
   
-  # driver
-  environment.systemPackages = with pkgs; [
-    logiops
-  ];
-  
   # configuration
   environment.etc = {
     "logid.cfg".source = ../etc/logid.cfg;
