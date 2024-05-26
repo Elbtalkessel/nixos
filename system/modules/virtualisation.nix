4b882c791b9514eb78b1058a562542eabdaac992
Elbtalkessel <rtfsc@pm.me>
2024-05-26 16:48:40 +0200

    Sun 26 May 2024 16:48:40 CEST

--- system/modules/virtualisation.nix
+++ system/modules/virtualisation.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
 
   # VIRTUALISATION
   # DOCKER / PODMAN
@@ -22,10 +22,26 @@
   # QEMU
   virtualisation.libvirtd = {
     enable = true;
+    onShutdown = "suspend";
+    onBoot = "ignore";
     qemu = {
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
       swtpm.enable = true;
+      runAsRoot = false;
     };
   };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    };
+  };
+
   programs.virt-manager.enable = true;
   # USERS
   # Define a user account. Don't forget to set a password with ‘passwd’.
