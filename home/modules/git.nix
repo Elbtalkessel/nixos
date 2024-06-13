dccf7b8488a12abcb61acc82b3b6da7e66d76454
Elbtalkessel <rtfsc@pm.me>
2024-06-13 14:45:53 +0200

    Thu 13 Jun 2024 14:45:53 CEST

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,17 +1,19 @@
-{ lib, ... }: {
-    programs.git = {
-      enable = true;
-      userName = "Elbtalkessel";
-      userEmail = "rtfsc@pm.me";
-      aliases = {
-        m = "merge --no-ff";
-        pp = "push";
-        p = "pull";
-        c = "commit";
-        s = "status";
-        a = "add .";
-        r = "reset --soft HEAD~1";
-        cdate = "!git add -A && git commit -m \"$(date)\"";
-      };
+_: {
+  programs.git = {
+    enable = true;
+    userName = "Elbtalkessel";
+    userEmail = "rtfsc@pm.me";
+    aliases = {
+      m = "merge --no-ff";
+      pp = "push";
+      p = "pull";
+      f = "fetch";
+      cc = "commit";
+      c = "checkout";
+      s = "status";
+      a = "add .";
+      r = "reset --soft HEAD~1";
+      cdate = "!git add -A && git commit -m \"$(date)\"";
     };
+  };
 }
