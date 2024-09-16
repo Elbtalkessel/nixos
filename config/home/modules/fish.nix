d2681bf53580ac2767832419d227b9001adb16c4
Elbtalkessel <rtfsc@pm.me>
2024-09-16 11:48:10 +0200

    Mon 16 Sep 2024 11:48:10 CEST

--- config/home/modules/fish.nix
+++ config/home/modules/fish.nix
@@ -15,6 +15,26 @@
 
       # trigger direnv at prompt only, this is similar functionality to the original behavior
       # set -g direnv_fish_mode disable_arrow
+
+      # This function allows you to switch to a different task
+      # when an interactive command takes too long
+      # by notifying you when it is finished.
+      #
+      # It is invoked by the fish shell automatically using its event system.
+      # src: https://github.com/kovidgoyal/kitty/issues/1892#issuecomment-1127515620
+      function __postexec_notify_on_long_running_commands --on-event fish_postexec
+         set --function interactive_commands 'nvim' 'mpv' 'man' 'less'
+         set --function command (string split ' ' $argv[1])
+         if contains $command $interactive_commands
+             # We quit interactive commands manually,
+             # no need for a notification.
+             return
+         end
+      
+         if test $CMD_DURATION -gt 5000
+             notify-send "Took $CMD_DURATION" "$argv"
+         end
+      end
     '';
     plugins = [
       {
