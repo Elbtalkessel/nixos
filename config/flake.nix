635d77f7fbef679dba4c5b576f37551b02db6bc1
Elbtalkessel <rtfsc@pm.me>
2024-10-24 09:01:16 +0200

    oof

--- config/flake.nix
+++ config/flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/master";
-    nixvim.url = "github:Elbtalkessel/nixvim/custom";
+    # Add a correct URL to the nix registry,
+    # `nix registry add <local or remote>`
+    # git+file:///.../nixvim is current value
+    nixvim.url = "flake:nixvim";
   };
 
   outputs =
