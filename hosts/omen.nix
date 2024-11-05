b49b8258e9c1c481710f2b2163ec7511eb8283d2
Elbtalkessel <rtfsc@pm.me>
2024-11-05 11:59:22 +0100

    Tue 05 Nov 2024 11:59:22 CET

--- hosts/omen.nix
+++ hosts/omen.nix
@@ -41,7 +41,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware = {
-    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    cpu.amd.updateMicrocode = true;
     nvidia = {
       open = true;
       prime = {
