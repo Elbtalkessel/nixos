315180f3c8d47ed4b27b58190f7dde9d55aeed34
Elbtalkessel <rtfsc@pm.me>
2024-11-08 10:33:21 +0100

    use zen kernel

--- system/configuration.nix
+++ system/configuration.nix
@@ -47,6 +47,14 @@ in
       # I have on average 400 processes running, double it and add a bit more just in case.
       "fs.inotify.max_user_instances" = "1024";
     };
+
+    # https://lore.kernel.org/io-uring/f4bfc61b-9fe6-466a-a943-7143ed1ec804@kernel.dk/T/
+    # Latest kernel 6.6.59 has an issue with the io_uring
+    # Two options, or use kernel 6.11.6 or zen kernel which is 6.11.5
+    # It seems switching to the zen kernel is the best solution as it forked from the stable kernel version
+    # Another option is to pin a specific kernel version:
+    # https://nixos.wiki/wiki/Linux_kernel
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   };
 
   sops = {
