f235c9e9f993bd54abbe1ab09b6ce480f065c078
Elbtalkessel <rtfsc@pm.me>
2025-02-05 09:36:05 +0100

    Wed 05 Feb 2025 09:36:05 CET

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,6 +1,7 @@
 _:
 let
   WALLPAPER = "/media/pictures/wallpaperflare.com_wallpaper (15).jpg";
+  AVATAR = "/media/pictures/D63NJrbO_2x.jpg";
 in
 {
   # Hypr* (land, paper, etc)
@@ -265,8 +266,8 @@ in
     enable = true;
     settings = {
       general = {
-        disable_loading_bar = true;
-        grace = 0;
+        disable_loading_bar = false;
+        grace = 30;
         hide_cursor = true;
         no_fade_in = false;
         ignore_empty_input = true;
@@ -280,19 +281,77 @@ in
         }
       ];
 
+      label = [
+        # Day
+        {
+          text = ''cmd[update:1000] echo -e "$(date +"%A")"'';
+          color = "rgba(216, 222, 233, 0.70)";
+          font_size = 90;
+          position = "0, 350";
+          halign = "center";
+          valign = "center";
+        }
+        # Date-Month
+        {
+          text = ''cmd[update:1000] echo -e "$(date +"%d %B")"'';
+          color = "rgba(216, 222, 233, 0.70)";
+          font_size = 40;
+          position = "0, 250";
+          halign = "center";
+          valign = "center";
+        }
+        # Time
+        {
+          text = ''cmd[update:1000] echo "<span>$(date +"- %I:%M -")</span>"'';
+          color = "rgba(216, 222, 233, 0.70)";
+          font_size = 20;
+          position = "0, 190";
+          halign = "center";
+          valign = "center";
+        }
+        # USER
+        {
+          text = "$USER";
+          color = "rgba(216, 222, 233, 0.80)";
+          font_size = 18;
+          position = "0, -130";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      # Profie-Photo
+      image = {
+        path = "${AVATAR}";
+        border_size = 2;
+        border_color = "rgba(255, 255, 255, .65)";
+        size = 130;
+        rounding = -1;
+        rotate = 0;
+        reload_time = -1;
+        position = "0, 40";
+        halign = "center";
+        valign = "center";
+      };
+
       input-field = [
         {
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
+          size = "300, 60";
+          outline_thickness = 2;
+          # Scale of input-field height, 0.2 - 0.8
+          dots_size = 0.2;
+          # Scale of dots' absolute size, 0.0 - 1.0
+          dots_spacing = 0.2;
           dots_center = true;
+          outer_color = "rgba(255, 255, 255, 0)";
+          inner_color = "rgba(255, 255, 255, 0.1)";
+          font_color = "rgb(200, 200, 200)";
           fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
-          shadow_passes = 2;
+          placeholder_text = ''<span foreground="##ffffff99">Enter Password</span>'';
+          hide_input = false;
+          position = "0, -210";
+          halign = "center";
+          valign = "center";
         }
       ];
     };
