0335a4f66ebb12678466ffe190b8e1fc8efb2ce2
Elbtalkessel <rtfsc@pm.me>
2025-02-10 09:59:48 +0100

    Mon 10 Feb 2025 09:59:48 CET

--- home/modules/waybar/window.nix
+++ home/modules/waybar/window.nix
@@ -1,6 +1,10 @@
 _: {
-  "format" = "{title:.256}";
+  "format" = "{title}";
   "max-length" = 100;
   "min-length" = 20;
   "align" = 0.5;
+  "tooltip-format" = "{}";
+  "icon" = true;
+  # Doesn't work yet, PR merged 5 days ago https://github.com/Alexays/Waybar/pull/3898.
+  "tooltip" = true;
 }
