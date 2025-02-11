4f7a6c5cf5be9a9d1c2707062697b195fccf960c
Elbtalkessel <rtfsc@pm.me>
2025-02-11 19:21:03 +0100

    Tue 11 Feb 2025 19:21:03 CET

--- devenv.nix
+++ devenv.nix
@@ -3,5 +3,13 @@ _: {
     chsecret.exec = ''
       nix-shell -p sops --run "sops system/secrets/secrets.yaml"
     '';
+    switch.exec = ''
+      if [ $(whoami) = "root" ]
+      then 
+        nixos-rebuild switch --flake ./
+      else
+        home-manager switch --flake ./
+      fi
+    '';
   };
 }
