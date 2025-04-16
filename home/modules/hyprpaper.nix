5337441ce45df713af19ad34f8de7bf5710850a1
Elbtalkessel <rtfsc@pm.me>
2025-04-16 19:54:31 +0200

    Wed 16 Apr 2025 19:54:31 CEST

--- home/modules/hyprpaper.nix
+++ home/modules/hyprpaper.nix
@@ -1,18 +1,16 @@
-_: {
+_:
+let
+  background = ../../assets/bg.png;
+in
+{
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
       splash_offset = 2.0;
-
-      preload = [
-        "${../../assets/bg.jpg}"
-      ];
-
-      wallpaper = [
-        ",${../../assets/bg.jpg}"
-      ];
+      preload = [ "${background}" ];
+      wallpaper = [ ",${background}" ];
     };
   };
 }
