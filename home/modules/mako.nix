2f148cb2e31d058bcfb06a2e62433aac30c4652d
Elbtalkessel <rtfsc@pm.me>
2025-05-04 15:02:55 +0200

    Sun 04 May 2025 15:02:55 CEST

--- home/modules/mako.nix
+++ home/modules/mako.nix
@@ -2,19 +2,23 @@ _: {
   # Requires libnotify
   services.mako = {
     enable = true;
-    backgroundColor = "#282828CC";
-    textColor = "#ebdbb2";
-    borderColor = "#32302f";
-    progressColor = "over #414559";
-    borderRadius = 10;
-    defaultTimeout = 5000;
-    font = "OverpassM Nerd Font 14";
-    extraConfig = "
-[urgency=low]
-default-timeout=2500
-[urgency=high]
-text-color=#fb4934
-default-timeout=0
-";
+    settings = {
+      backgroundColor = "#282828CC";
+      textColor = "#ebdbb2";
+      borderColor = "#32302f";
+      progressColor = "over #414559";
+      borderRadius = "10";
+      defaultTimeout = "5000";
+      font = "OverpassM Nerd Font 14";
+    };
+    criterias = {
+      "urgency=low" = {
+        defaultTimeout = "2500";
+      };
+      "urgency=high" = {
+        textColor = "#fb4934";
+        defaultTimeout = "0";
+      };
+    };
   };
 }
