8010f626009058accdd51acdca622382adfc60b9
Elbtalkessel <rtfsc@pm.me>
2025-06-12 08:50:54 +0200

    Thu 12 Jun 2025 08:50:54 CEST

--- system/modules/webdav.nix
+++ system/modules/webdav.nix
@@ -5,14 +5,24 @@ _: {
   systemd.mounts = [
     {
       enable = true;
-      description = "Moon's webdav";
+      description = "Moon's root webdav";
       after = [ "network-online.target" ];
       wants = [ "network-online.target" ];
-      what = "https://192.168.1.90:9802";
-      where = "/mnt/share/moon";
-      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
+      what = "http://192.168.1.90:9800";
+      where = "/mnt/share";
+      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775,x-systemd.automount";
       type = "davfs";
       mountConfig.TimeoutSec = 15;
+    }
+  ];
+  systemd.automounts = [
+    {
+      description = "Moon's root webdav";
+      where = "/mnt/share";
+      wantedBy = [ "multi-user.target" ];
+      automountConfig = {
+        TimeoutIdleSec = "2m";
+      };
     }
   ];
 }
