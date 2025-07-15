e02deeda5df9612f7b9a802fdadd44f0e8792dd5
Elbtalkessel <rtfsc@pm.me>
2025-07-15 16:38:06 +0200

    small improvement

--- home/bin/setbg.sh
+++ home/bin/setbg.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 cache="/home/risus/.cache/wallpaper"
-if [ "$1" != "$cache" ]
+if [ -n "$1" ] && [ ! -f "$1" ]
+then
+  exit 1
+fi
+
+if [ -f "$1" ]
 then
   ln -sf "$1" "$cache" 2>/dev/null
 fi
