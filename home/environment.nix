4a0e689a168d0e04c41793250f7355afe852de1a
Elbtalkessel <rtfsc@pm.me>
2025-07-16 09:45:53 +0200

    use config instead of hardcoded values

--- home/environment.nix
+++ home/environment.nix
@@ -1,23 +1,23 @@
-{ options, ... }:
+{ config, ... }:
 let
-  HOME = "/home/${options.user}";
+  HOME = "/home/${config.user}";
   sessionVariables = {
     XDG_DATA_HOME = "${HOME}/.local/share";
     XDG_CONFIG_HOME = "${HOME}/.config";
     XDG_CACHE_HOME = "${HOME}/.cache";
     XDG_DOWNLOAD_DIR = "${HOME}/Downloads";
-    XDG_RUNTIME_DIR = "/run/user/1000";
+    XDG_RUNTIME_DIR = "/run/${config.user}/1000";
     XDG_DESKTOP_DIR = "${HOME}/Desktop";
     XDG_DOCUMENTS_DIR = "${HOME}/Documents";
-    XDG_MUSIC_DIR = "/mnt/share/Music";
+    XDG_MUSIC_DIR = "${HOME}/Music";
     XDG_PICTURES_DIR = "${HOME}/Pictures";
     XDG_PUBLICSHARE_DIR = "${HOME}/Public";
     XDG_TEMPLATES_DIR = "${HOME}/Templates";
     XDG_VIDEOS_DIR = "${HOME}/Videos";
     XDG_DATA_DIRS = "${HOME}/.local/share/flatpak/exports/share";
-    EDITOR = "nvim";
-    TERMINAL = "alacritty";
-    BROWSER = "vivaldi";
+    EDITOR = config.editor;
+    TERMINAL = config.terminal;
+    BROWSER = config.browser;
     VAGRANT_DEFAULT_PROVIDER = "kvm";
     VIRSH_DEFAULT_CONNECT_URI = "qemu:///system";
     DOCKER_BUILDKIT = "1";
