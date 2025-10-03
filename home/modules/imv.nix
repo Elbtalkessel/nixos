f4cd8072c6409c3ad89202cdc2796040ada048ca
Elbtalkessel <rtfsc@pm.me>
2025-10-03 13:55:07 +0200

    Fri 03 Oct 2025 13:55:07 CEST

--- home/modules/imv.nix
+++ home/modules/imv.nix
@@ -2,13 +2,15 @@ _: {
   programs.imv = {
     enable = true;
     settings = {
+      # Note: on wayland a command may run infinite times
+      # if the imv window unfocuses.
+      # https://todo.sr.ht/~exec64/imv/7
+      # https://github.com/eXeC64/imv/issues/207#issuecomment-607831458
+      # Only workaround is to run it in background &
       binds = {
-        q = "quit";
-        l = "next";
-        h = "prev";
-        y = "exec wl-copy \"$imv_current_file\"";
-        u = "exec setbg \"$imv_current_file\"";
-        d = "exec rm \"$imv_current_file\"";
+        y = ''exec wl-copy "$imv_current_file"'';
+        u = ''exec setbg "$imv_current_file" &'';
+        D = ''exec (rm "$imv_current_file"; imv-msg $imv_pid next) &'';
       };
     };
   };
