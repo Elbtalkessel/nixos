1ce22ce74ad63903625781ca954489f82ba51485
Elbtalkessel <rtfsc@pm.me>
2025-11-13 13:15:40 +0100

    vicinae and hyprpanel settings

--- home/modules/bar/hyprpanel/default.nix
+++ home/modules/bar/hyprpanel/default.nix
@@ -1,6 +1,124 @@
 _: {
-  imports = [
-    ./settings.nix
-    ./theme-experiment.nix
-  ];
+  programs.hyprpanel = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      "theme.font.name" = "OverpassM Nerd Font";
+      "theme.font.label" = "OverpassM Nerd Font";
+      "theme.font.size" = "1rem";
+      "menus.transition" = "none";
+      "bar.layouts" = {
+        "0" = {
+          "left" = [
+            "dashboard"
+            "workspaces"
+          ];
+          "middle" = [
+            "clock"
+            "windowtitle"
+          ];
+          "right" = [
+            "media"
+            "volume"
+            "network"
+            "bluetooth"
+            "battery"
+            "notifications"
+            "systray"
+          ];
+        };
+      };
+      "theme.bar.floating" = false;
+      "theme.bar.margin_top" = "0";
+      "theme.bar.margin_sides" = "0";
+      "theme.bar.margin_bottom" = "0";
+      "bar.launcher.autoDetectIcon" = true;
+      "bar.workspaces.monitorSpecific" = false;
+      "bar.workspaces.show_icons" = false;
+      "bar.workspaces.show_numbered" = true;
+      "bar.workspaces.workspaces" = 10;
+      "bar.volume.label" = false;
+      "bar.network.label" = false;
+      "bar.network.truncation" = false;
+      "bar.bluetooth.label" = false;
+      "bar.battery.hideLabelWhenFull" = true;
+      "theme.bar.buttons.systray.enableBorder" = false;
+      "bar.clock.format" = "%I:%M %p";
+      "bar.clock.showIcon" = false;
+      "bar.clock.showTime" = true;
+      "bar.media.show_label" = true;
+      "bar.media.show_active_only" = true;
+      "bar.notifications.hideCountWhenZero" = true;
+      "theme.notification.enableShadow" = true;
+      "notifications.showActionsOnHover" = false;
+      "menus.volume.raiseMaximumVolume" = true;
+      "menus.clock.time.military" = true;
+      "menus.clock.weather.enabled" = false;
+      "menus.clock.weather.unit" = "metric";
+      "bar.customModules.storage.paths" = [
+        "/"
+      ];
+      "theme.bar.menus.monochrome" = true;
+      "theme.bar.menus.border.size" = "0.1em";
+      "theme.bar.transparent" = false;
+      "theme.bar.opacity" = 50;
+      "theme.bar.buttons.background_opacity" = 0;
+      "theme.bar.buttons.monochrome" = false;
+      "theme.bar.outer_spacing" = "0";
+      "theme.bar.buttons.y_margins" = "0";
+      "theme.bar.scaling" = 85;
+      "theme.tooltip.scaling" = 85;
+      "theme.bar.menus.popover.scaling" = 75;
+      "theme.bar.menus.switch.radius" = "5em";
+      "theme.bar.menus.switch.slider_radius" = "5em";
+      "theme.bar.menus.slider.slider_radius" = "5rem";
+      "theme.bar.menus.slider.progress_radius" = "5rem";
+      "theme.bar.menus.scroller.radius" = "0.7em";
+      "theme.bar.menus.progressbar.radius" = "5rem";
+      "theme.bar.menus.buttons.radius" = "0.7em";
+      "theme.bar.buttons.opacity" = 100;
+      "theme.bar.buttons.background_hover_opacity" = 100;
+      "menus.dashboard.powermenu.avatar.image" = "/home/risus/Pictures/Horny/cheeky-schoolgirl.jpg";
+      "theme.bar.menus.menu.dashboard.profile.radius" = "5em";
+      "theme.bar.menus.menu.dashboard.scaling" = 85;
+      "bar.windowtitle.custom_title" = false;
+      "theme.bar.buttons.windowtitle.enableBorder" = false;
+      "bar.windowtitle.class_name" = false;
+      "bar.windowtitle.label" = true;
+      "bar.windowtitle.icon" = true;
+      "wallpaper.enable" = false;
+      "theme.bar.menus.border.radius" = "0.7em";
+      "theme.bar.buttons.spacing" = "0";
+      "theme.bar.buttons.padding_x" = "0.34rem";
+      "bar.battery.label" = false;
+      "menus.dashboard.shortcuts.left.shortcut1.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut1.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut1.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.tooltip" = "";
+      "menus.dashboard.shortcuts.right.shortcut1.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.icon" = "";
+      "menus.dashboard.powermenu.avatar.name" = "system";
+      "theme.bar.menus.menu.dashboard.profile.size" = "11em";
+      "menus.dashboard.directories.enabled" = false;
+      "menus.dashboard.powermenu.sleep" = "systemctl suspend";
+      "menus.dashboard.powermenu.logout" = "hyprctl dispatch exit";
+      "menus.dashboard.powermenu.reboot" = "systemctl reboot";
+      "menus.dashboard.powermenu.shutdown" = "systemctl poweroff";
+      "theme.bar.menus.background" = "#1a1a1a";
+      "theme.bar.menus.cards" = "#1a1a1a";
+      "theme.bar.menus.text" = "#cdd6f4";
+      "theme.bar.buttons.style" = "default";
+      "theme.bar.buttons.icon_background" = "#242438";
+      "theme.bar.menus.buttons.default" = "#b4befe";
+      "theme.bar.menus.iconbuttons.passive" = "#cdd6f3";
+      "theme.bar.menus.iconbuttons.active" = "#b4beff";
+    };
+  };
 }
--- home/modules/launcher/vicinae.nix
+++ home/modules/launcher/vicinae.nix
@@ -11,22 +11,37 @@
       enable = true;
     };
     settings = {
+      closeOnFocusLoss = false;
+      faviconService = "google";
       font = {
         normal = config.my.font-family;
         size = 10;
       };
       keybindings = "default";
+      keybinds = { };
+
+      popToRootOnClose = true;
+      rootSearch = {
+        searchFiles = true;
+      };
+      theme = {
+        name = "vicinae-dark";
+      };
       window = {
         csd = true;
         inherit (config.my) opacity;
+        rounding = 10;
       };
     };
   };
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/vicinae.nix
   # https://docs.vicinae.com/launcher-window
+  # Settings the layer shell is workaround:
+  # https://github.com/vicinaehq/vicinae/issues/398
+  # No way to set scaling without custom env.
   systemd.user.services.vicinae.Service.EnvironmentFile = lib.mkForce (
     pkgs.writeText "vicinae-env" ''
-      USE_LAYER_SHELL=1
+      USE_LAYER_SHELL=0
       QT_SCALE_FACTOR=1.5
     ''
   );
