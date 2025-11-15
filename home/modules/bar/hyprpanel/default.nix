7ac628e26090a7b7927f99f579179c629b060b83
Elbtalkessel <rtfsc@pm.me>
2025-11-15 13:59:32 +0100

    update hyprpanel theme

--- home/modules/bar/hyprpanel/default.nix
+++ home/modules/bar/hyprpanel/default.nix
@@ -24,123 +24,150 @@
     enable = true;
     systemd.enable = true;
     settings = {
-      "theme.font.name" = "OverpassM Nerd Font";
-      "theme.font.label" = "OverpassM Nerd Font";
-      "theme.font.size" = "1rem";
-      "menus.transition" = "none";
+      "bar.battery.hideLabelWhenFull" = true;
+      "bar.battery.label" = true;
+      "bar.bluetooth.label" = false;
+      "bar.clock.format" = "%I:%M %p";
+      "bar.clock.showIcon" = true;
+      "bar.clock.showTime" = true;
+      "bar.customModules.storage.paths" = [
+        "/"
+      ];
+      "bar.launcher.autoDetectIcon" = true;
       "bar.layouts" = {
         "0" = {
           "left" = [
             "workspaces"
             "clock"
+            "systray"
           ];
           "middle" = [
             "windowtitle"
-            "media"
           ];
           "right" = [
+            "media"
             "volume"
             "network"
-            "bluetooth"
             "battery"
+            "kbinput"
+            "bluetooth"
             "notifications"
             "dashboard"
-            "kbinput"
           ];
         };
       };
-      "theme.bar.floating" = false;
-      "theme.bar.margin_top" = "0";
-      "theme.bar.margin_sides" = "0";
-      "theme.bar.margin_bottom" = "0";
-      "bar.launcher.autoDetectIcon" = true;
-      "bar.workspaces.monitorSpecific" = false;
-      "bar.workspaces.show_icons" = false;
-      "bar.workspaces.show_numbered" = true;
-      "bar.workspaces.workspaces" = 10;
-      "bar.volume.label" = false;
+      "bar.media.show_active_only" = true;
+      "bar.media.show_label" = true;
+      "bar.media.truncation" = true;
       "bar.network.label" = true;
       "bar.network.truncation" = false;
-      "bar.bluetooth.label" = false;
-      "bar.battery.hideLabelWhenFull" = true;
-      "theme.bar.buttons.systray.enableBorder" = false;
-      "bar.clock.format" = "%I:%M %p";
-      "bar.clock.showIcon" = false;
-      "bar.clock.showTime" = true;
-      "bar.media.show_label" = true;
-      "bar.media.show_active_only" = true;
-      "bar.media.truncation" = true;
       "bar.notifications.hideCountWhenZero" = true;
-      "theme.notification.enableShadow" = true;
-      "notifications.showActionsOnHover" = false;
-      "menus.volume.raiseMaximumVolume" = true;
+      "bar.volume.label" = true;
+      "bar.windowtitle.class_name" = false;
+      "bar.windowtitle.custom_title" = false;
+      "bar.windowtitle.icon" = true;
+      "bar.windowtitle.label" = true;
+      "bar.workspaces.monitorSpecific" = false;
+      "bar.workspaces.show_icons" = false;
+      "bar.workspaces.show_numbered" = false;
+      "bar.workspaces.workspaces" = 10;
       "menus.clock.time.military" = true;
       "menus.clock.weather.enabled" = false;
       "menus.clock.weather.unit" = "metric";
-      "bar.customModules.storage.paths" = [
-        "/"
-      ];
-      "theme.bar.menus.monochrome" = true;
-      "theme.bar.menus.border.size" = "0.1em";
-      "theme.bar.transparent" = false;
-      "theme.bar.opacity" = 50;
-      "theme.bar.buttons.background_opacity" = 0;
-      "theme.bar.buttons.monochrome" = false;
-      "theme.bar.outer_spacing" = "0";
-      "theme.bar.buttons.y_margins" = "0";
-      "theme.bar.scaling" = 85;
-      "theme.tooltip.scaling" = 85;
-      "theme.bar.menus.popover.scaling" = 75;
-      "theme.bar.menus.switch.radius" = "5em";
-      "theme.bar.menus.switch.slider_radius" = "5em";
-      "theme.bar.menus.slider.slider_radius" = "5rem";
-      "theme.bar.menus.slider.progress_radius" = "5rem";
-      "theme.bar.menus.scroller.radius" = "0.7em";
-      "theme.bar.menus.progressbar.radius" = "5rem";
-      "theme.bar.menus.buttons.radius" = "0.7em";
-      "theme.bar.buttons.opacity" = 100;
-      "theme.bar.buttons.background_hover_opacity" = 100;
-      "menus.dashboard.powermenu.avatar.image" = "/home/risus/Pictures/Horny/cheeky-schoolgirl.jpg";
-      "theme.bar.menus.menu.dashboard.profile.radius" = "5em";
-      "theme.bar.menus.menu.dashboard.scaling" = 85;
-      "bar.windowtitle.custom_title" = false;
-      "theme.bar.buttons.windowtitle.enableBorder" = false;
-      "bar.windowtitle.class_name" = false;
-      "bar.windowtitle.label" = true;
-      "bar.windowtitle.icon" = false;
-      "wallpaper.enable" = false;
-      "theme.bar.menus.border.radius" = "0.7em";
-      "theme.bar.buttons.spacing" = "0";
-      "theme.bar.buttons.padding_x" = "0.34rem";
-      "bar.battery.label" = false;
-      "menus.dashboard.shortcuts.left.shortcut1.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut1.command" = "";
-      "menus.dashboard.shortcuts.left.shortcut1.tooltip" = "";
-      "menus.dashboard.shortcuts.left.shortcut2.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut2.command" = "";
-      "menus.dashboard.shortcuts.left.shortcut2.tooltip" = "";
-      "menus.dashboard.shortcuts.left.shortcut3.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut3.command" = "";
-      "menus.dashboard.shortcuts.left.shortcut3.tooltip" = "";
-      "menus.dashboard.shortcuts.left.shortcut4.tooltip" = "";
-      "menus.dashboard.shortcuts.right.shortcut1.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut4.command" = "";
-      "menus.dashboard.shortcuts.left.shortcut4.icon" = "";
-      "menus.dashboard.powermenu.avatar.name" = "system";
-      "theme.bar.menus.menu.dashboard.profile.size" = "11em";
       "menus.dashboard.directories.enabled" = false;
-      "menus.dashboard.powermenu.sleep" = "systemctl suspend";
+      "menus.dashboard.powermenu.avatar.image" = "/home/risus/Pictures/Horny/cheeky-schoolgirl.jpg";
+      "menus.dashboard.powermenu.avatar.name" = "system";
       "menus.dashboard.powermenu.logout" = "hyprctl dispatch exit";
       "menus.dashboard.powermenu.reboot" = "systemctl reboot";
       "menus.dashboard.powermenu.shutdown" = "systemctl poweroff";
-      "theme.bar.menus.background" = "#1a1a1a";
-      "theme.bar.menus.cards" = "#1a1a1a";
-      "theme.bar.menus.text" = "#cdd6f4";
-      "theme.bar.buttons.style" = "default";
+      "menus.dashboard.powermenu.sleep" = "systemctl suspend";
+      "menus.dashboard.shortcuts.left.shortcut1.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut1.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut1.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut2.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut3.tooltip" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.command" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.icon" = "";
+      "menus.dashboard.shortcuts.left.shortcut4.tooltip" = "";
+      "menus.dashboard.shortcuts.right.shortcut1.icon" = "";
+      "menus.transition" = "none";
+      "menus.volume.raiseMaximumVolume" = true;
+      "notifications.showActionsOnHover" = false;
+      "theme.bar.buttons.background_hover_opacity" = 100;
+      "theme.bar.buttons.background_opacity" = 75;
       "theme.bar.buttons.icon_background" = "#242438";
+      "theme.bar.buttons.monochrome" = true;
+      "theme.bar.buttons.opacity" = 75;
+      "theme.bar.buttons.padding_x" = "0.4rem";
+      "theme.bar.buttons.spacing" = "0.3em";
+      "theme.bar.buttons.style" = "default";
+      "theme.bar.buttons.systray.enableBorder" = false;
+      "theme.bar.buttons.windowtitle.enableBorder" = false;
+      "theme.bar.buttons.y_margins" = "0.2em";
+      "theme.bar.floating" = false;
+      "theme.bar.margin_bottom" = "0";
+      "theme.bar.margin_sides" = "0";
+      "theme.bar.margin_top" = "0";
+      "theme.bar.menus.background" = "#1a1a1a";
+      "theme.bar.menus.border.radius" = "0.7em";
+      "theme.bar.menus.border.size" = "0.1em";
       "theme.bar.menus.buttons.default" = "#b4befe";
-      "theme.bar.menus.iconbuttons.passive" = "#cdd6f3";
+      "theme.bar.menus.buttons.radius" = "0.7em";
+      "theme.bar.menus.cards" = "#1a1a1a";
       "theme.bar.menus.iconbuttons.active" = "#b4beff";
+      "theme.bar.menus.iconbuttons.passive" = "#cdd6f3";
+      "theme.bar.menus.menu.dashboard.profile.radius" = "5em";
+      "theme.bar.menus.menu.dashboard.profile.size" = "11em";
+      "theme.bar.menus.menu.dashboard.scaling" = 85;
+      "theme.bar.menus.monochrome" = true;
+      "theme.bar.menus.popover.scaling" = 75;
+      "theme.bar.menus.progressbar.radius" = "5rem";
+      "theme.bar.menus.scroller.radius" = "0.7em";
+      "theme.bar.menus.slider.progress_radius" = "5rem";
+      "theme.bar.menus.slider.slider_radius" = "5rem";
+      "theme.bar.menus.switch.radius" = "5em";
+      "theme.bar.menus.switch.slider_radius" = "5em";
+      "theme.bar.menus.text" = "#cdd6f4";
+      "theme.bar.opacity" = 100;
+      "theme.bar.outer_spacing" = "0";
+      "theme.bar.scaling" = 85;
+      "theme.bar.transparent" = true;
+      "theme.font.label" = "OverpassM Nerd Font";
+      "theme.font.name" = "OverpassM Nerd Font";
+      "theme.font.size" = "1rem";
+      "theme.notification.enableShadow" = true;
+      "theme.tooltip.scaling" = 85;
+      "wallpaper.enable" = false;
+      "bar.workspaces.showWsIcons" = true;
+      "bar.workspaces.showApplicationIcons" = true;
+      "bar.workspaces.applicationIconEmptyWorkspace" = "";
+      "wallpaper.image" = "/home/risus/.cache/wallpaper";
+      "theme.matugen" = false;
+      "theme.matugen_settings.scheme_type" = "content";
+      "theme.matugen_settings.variation" = "standard_3";
+      "theme.bar.buttons.radius" = "0.5em";
+      "theme.bar.buttons.padding_y" = "0";
+      "bar.media.truncation_size" = 20;
+      "bar.media.format" = "{title}";
+      "theme.bar.buttons.clock.spacing" = "0.35em";
+      "theme.bar.buttons.notifications.spacing" = "0.35em";
+      "theme.bar.buttons.media.spacing" = "0.35em";
+      "theme.bar.buttons.battery.spacing" = "0.35em";
+      "theme.bar.buttons.bluetooth.spacing" = "0.35em";
+      "theme.bar.buttons.network.spacing" = "0.35em";
+      "theme.bar.buttons.volume.spacing" = "0.35em";
+      "theme.bar.buttons.windowtitle.spacing" = "0.5em";
+      "theme.bar.buttons.background" = "#242438";
+      "theme.bar.buttons.text" = "#deddda";
+      "theme.bar.buttons.icon" = "#9a9996";
+      "theme.bar.menus.label" = "#ffffff";
+      "theme.bar.menus.card_radius" = "0.5em";
+      "theme.bar.buttons.workspaces.available" = "#9a9996";
+      "theme.bar.buttons.workspaces.occupied" = "#ffffff";
     };
   };
 }
--- home/modules/bar/hyprpanel/settings/theme.nix
+++ home/modules/bar/hyprpanel/settings/theme.nix
@@ -1,1 +1,14 @@
-
+{ config, ... }:
+{
+  font = {
+    name = config.my.font-family;
+    label = config.my.font-family;
+    size = "1rem";
+  };
+  bar = {
+    floating = false;
+    margin_top = "0";
+    margin_sides = "0";
+    margin_bottom = "0";
+  };
+}
