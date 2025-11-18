98a31475121865a15cc40146b05184a37f5c54b1
Elbtalkessel <rtfsc@pm.me>
2025-11-18 10:56:41 +0100

    remove bottles, fix hardwared

--- home/modules/game.nix
+++ home/modules/game.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (bottles.override { removeWarningPopup = true; })
+    # bottles installed using flatpak
     discord
+    # escape hatch, bottles is able to install dependencies
+    # as well, but the number of those is limited.
     winetricks
+    # required for winetricks.
     # native wayland support (unstable), there are a number
     # of other variants, see https://nixos.wiki/wiki/Wine
     wineWowPackages.waylandFull
--- system/modules/steam.nix
+++ system/modules/steam.nix
@@ -7,6 +7,6 @@ _: {
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
       gamescopeSession.enable = true;
     };
-    hardware.xone.enable = true;
   };
+  hardware.xone.enable = true;
 }
