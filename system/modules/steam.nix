43a9ca3cf1513aa2f31ce6896b593dcfb91b39b9
Elbtalkessel <rtfsc@pm.me>
2025-11-21 09:32:27 +0100

    close ports

--- system/modules/steam.nix
+++ system/modules/steam.nix
@@ -1,12 +1,26 @@
-_: {
+{ pkgs, ... }:
+{
   programs = {
     steam = {
       enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-      gamescopeSession.enable = true;
+      # Open ports in the firewall for Steam Remote Play
+      remotePlay.openFirewall = false;
+      # Open ports in the firewall for Source Dedicated Server
+      dedicatedServer.openFirewall = false;
+      # Open ports in the firewall for Steam Local Network Game Transfers
+      localNetworkGameTransfers.openFirewall = false;
+      gamescopeSession.enable = false;
+      extraPackages = with pkgs; [
+        qogir-icon-theme
+        steamtinkerlaunch
+      ];
+      extraCompatPackages = with pkgs; [
+        steamtinkerlaunch
+      ];
+    };
+    gamescope = {
+      enable = true;
+      capSysNice = true;
     };
   };
-  hardware.xone.enable = true;
 }
