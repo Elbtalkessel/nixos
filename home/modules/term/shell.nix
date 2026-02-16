c528d678d88dae033794ec2ce83e6ceb72524520
Elbtalkessel <rtfsc@pm.me>
2026-02-16 12:35:30 +0100

    Mon 16 Feb 2026 12:35:30 CET

--- home/modules/term/shell.nix
+++ home/modules/term/shell.nix
@@ -112,8 +112,8 @@ in
           "$username"
           "$hostname"
           "$directory"
-          "$git_branch"
           "$git_state"
+          "$git_branch"
           "$git_status"
           "$cmd_duration"
           "$line_break"
@@ -122,6 +122,34 @@ in
         ];
         directory = {
           style = "blue";
+        };
+        git_state = {
+          format = ''(\([$state( $progress_current/$progress_total)]($style)\) )'';
+          style = "bright-black";
+        };
+        git_branch = {
+          format = "[$branch]($style)";
+          style = "bright-black";
+        };
+        git_status = {
+          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218)($ahead_behind$stashed)]($style)";
+          style = "cyan";
+          conflicted = "​";
+          untracked = "​";
+          modified = "​";
+          staged = "​";
+          renamed = "​";
+          deleted = "​";
+          stashed = "≡";
+        };
+        cmd_duration = {
+          format = "( [$duration]($style))";
+          style = "yellow";
+        };
+        # ---
+        python = {
+          format = "([$virtualenv]($style) )";
+          style = "bright-black";
         };
         shell = {
           disabled = false;
@@ -134,33 +162,6 @@ in
           success_symbol = "[_](purple)";
           error_symbol = "[_](red)";
           vimcmd_symbol = "[>](green)";
-        };
-        git_branch = {
-          format = "[$branch]($style)";
-          style = "bright-black";
-        };
-        git_status = {
-          format = "([[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style) )";
-          style = "cyan";
-          conflicted = "​";
-          untracked = "​";
-          modified = "​";
-          staged = "​";
-          renamed = "​";
-          deleted = "​";
-          stashed = "≡";
-        };
-        git_state = {
-          format = ''(\([$state( $progress_current/$progress_total)]($style)\) )'';
-          style = "bright-black";
-        };
-        cmd_duration = {
-          format = "([$duration]($style) )";
-          style = "yellow";
-        };
-        python = {
-          format = "([$virtualenv]($style) )";
-          style = "bright-black";
         };
       };
     };
