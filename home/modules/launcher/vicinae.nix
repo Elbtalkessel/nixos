c5ec04ccf0f758314d6fb6c3644493c5018cc681
Elbtalkessel <rtfsc@pm.me>
2026-06-17 18:58:23 +0200

    integrate hyprshutdown, use uwsm to launch apps

--- home/modules/launcher/vicinae.nix
+++ home/modules/launcher/vicinae.nix
@@ -10,53 +10,61 @@
     systemd = {
       enable = true;
     };
-    settings = {
-      font = {
-        normal = {
-          family = config.my.font.family.default;
-          size = 11;
+    settings =
+      let
+        launchPrefix = if config.my.wm.uwsm.enable then "uwsm-app --" else "";
+      in
+      {
+        font = {
+          normal = {
+            family = config.my.font.family.default;
+            size = 11;
+          };
+          rendering = "qt";
         };
-        rendering = "qt";
-      };
-      launcher_window = {
-        opacity = 0.9;
-      };
-      providers = {
-        power = {
-          entrypoints =
-            let
-              hyprshutdown = lib.getExe pkgs.hyprshutdown;
-              run = cmd: if config.my.wm.uwsm.enable then "uwsm app -- ${cmd}" else cmd;
-            in
-            {
-              logout = {
-                preferences = {
-                  confirm = true;
-                  customProgram = run "${hyprshutdown} -t 'Logging out...'";
-                };
-              };
-              power-off = {
-                preferences = {
-                  confirm = true;
-                  customProgram = run "${hyprshutdown} -t 'Shutting down...' --post-cmd 'shutdown -P now'";
-                };
-              };
-              reboot = {
-                preferences = {
-                  confirm = true;
-                  customProgram = run "${hyprshutdown} -t 'Restarting...' --post-cmd 'reboot'";
-                };
-              };
+        launcher_window = {
+          opacity = 0.97;
+        };
+        providers = {
+          applications = {
+            preferences = {
+              inherit launchPrefix;
             };
+          };
+          power = {
+            entrypoints =
+              let
+                hyprshutdown = lib.getExe pkgs.hyprshutdown;
+              in
+              {
+                logout = {
+                  preferences = {
+                    confirm = true;
+                    customProgram = "${hyprshutdown} -t 'Logging out...'";
+                  };
+                };
+                power-off = {
+                  preferences = {
+                    confirm = true;
+                    customProgram = "${hyprshutdown} -t 'Shutting down...' --post-cmd 'shutdown -P now'";
+                  };
+                };
+                reboot = {
+                  preferences = {
+                    confirm = true;
+                    customProgram = "${hyprshutdown} -t 'Restarting...' --post-cmd 'reboot'";
+                  };
+                };
+              };
+          };
+        };
+        theme = {
+          dark = {
+            icon_theme = "Adwaita";
+            name = "kanagawa";
+          };
         };
       };
-      theme = {
-        dark = {
-          icon_theme = "Adwaita";
-          name = "kanagawa";
-        };
-      };
-    };
   };
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/vicinae.nix
   # https://docs.vicinae.com/launcher-window
