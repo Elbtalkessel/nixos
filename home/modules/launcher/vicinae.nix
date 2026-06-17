b7917b73b2ce77f12294d2cff3cf252096d3917b
Elbtalkessel <rtfsc@pm.me>
2026-06-17 11:18:43 +0200

    move vicinae config

--- home/modules/launcher/vicinae.nix
+++ home/modules/launcher/vicinae.nix
@@ -1,9 +1,61 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   programs.vicinae = {
     enable = true;
     systemd = {
       enable = true;
+    };
+    settings = {
+      font = {
+        normal = {
+          family = config.my.font.family.default;
+          size = 11;
+        };
+        rendering = "qt";
+      };
+      launcher_window = {
+        opacity = 0.9;
+      };
+      providers = {
+        power = {
+          entrypoints =
+            let
+              hyprshutdown = lib.getExe pkgs.hyprshutdown;
+              run = cmd: if config.my.wm.uwsm.enable then "uwsm app -- ${cmd}" else cmd;
+            in
+            {
+              logout = {
+                preferences = {
+                  confirm = true;
+                  customProgram = run "${hyprshutdown} -t 'Logging out...'";
+                };
+              };
+              power-off = {
+                preferences = {
+                  confirm = true;
+                  customProgram = run "${hyprshutdown} -t 'Shutting down...' --post-cmd 'shutdown -P now'";
+                };
+              };
+              reboot = {
+                preferences = {
+                  confirm = true;
+                  customProgram = run "${hyprshutdown} -t 'Restarting...' --post-cmd 'reboot'";
+                };
+              };
+            };
+        };
+      };
+      theme = {
+        dark = {
+          icon_theme = "Adwaita";
+          name = "kanagawa";
+        };
+      };
     };
   };
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/vicinae.nix
