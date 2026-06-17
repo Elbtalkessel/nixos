d8befa3459cfb1f019c3f6018131293fbfc21f9a
Elbtalkessel <rtfsc@pm.me>
2026-06-17 21:08:20 +0200

    Wed 17 Jun 2026 21:08:20 CEST

--- home/modules/hypr/hyprland/hyprexpo.lua
+++ home/modules/hypr/hyprland/hyprexpo.lua
@@ -1,9 +1,25 @@
 -- https://hyprexpo.lol/docs/configuration/options
 
+local function toRGB(color)
+	local hex = color:gsub("#", "")
+	local func = (#hex == 8) and "rgba" or "rgb"
+	return string.format("%s(%s)", func, hex)
+end
+
 hl.config({
 	plugin = {
 		hyprexpo = {
 			columns = 3,
+			max_workspace = 9,
+			show_cursor = 0,
+			bg_col = toRGB(M.COLOR.BG_SURFACE),
+			border_color_focus = toRGB(M.COLOR.BG_PRIMARY_CONTAINER),
+			drag_drop_proxy_color = toRGB(M.COLOR.BG_PRIMARY_CONTAINER .. "42"),
+			drag_drop_proxy_active_color = toRGB(M.COLOR.BG_SECONDARY_CONTAINER .. "42"),
+			border_width = 1,
+			border_color_current = "",
+			border_color_hover = "",
+			tile_rounding = 4,
 		},
 	},
 })
