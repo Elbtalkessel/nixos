756d640eb1116ec4f3ee55d20776e188eb42f7c5
Elbtalkessel <rtfsc@pm.me>
2026-06-17 19:05:51 +0200

    styling

--- home/modules/launcher/vicinae.nix
+++ home/modules/launcher/vicinae.nix
@@ -4,67 +4,63 @@
   config,
   ...
 }:
+let
+  launchPrefix = if config.my.wm.uwsm.enable then "uwsm-app --" else "";
+  hyprshutdown = lib.getExe pkgs.hyprshutdown;
+in
 {
   programs.vicinae = {
     enable = true;
     systemd = {
       enable = true;
     };
-    settings =
-      let
-        launchPrefix = if config.my.wm.uwsm.enable then "uwsm-app --" else "";
-      in
-      {
-        font = {
-          normal = {
-            family = config.my.font.family.default;
-            size = 11;
-          };
-          rendering = "qt";
+    settings = {
+      font = {
+        normal = {
+          family = config.my.font.family.default;
+          size = 11;
         };
-        launcher_window = {
-          opacity = 0.97;
-        };
-        providers = {
-          applications = {
-            preferences = {
-              inherit launchPrefix;
-            };
+        rendering = "qt";
+      };
+      launcher_window = {
+        opacity = 0.97;
+      };
+      providers = {
+        applications = {
+          preferences = {
+            inherit launchPrefix;
           };
-          power = {
-            entrypoints =
-              let
-                hyprshutdown = lib.getExe pkgs.hyprshutdown;
-              in
-              {
-                logout = {
-                  preferences = {
-                    confirm = true;
-                    customProgram = "${hyprshutdown} -t 'Logging out...'";
-                  };
-                };
-                power-off = {
-                  preferences = {
-                    confirm = true;
-                    customProgram = "${hyprshutdown} -t 'Shutting down...' --post-cmd 'shutdown -P now'";
-                  };
-                };
-                reboot = {
-                  preferences = {
-                    confirm = true;
-                    customProgram = "${hyprshutdown} -t 'Restarting...' --post-cmd 'reboot'";
-                  };
-                };
+        };
+        power = {
+          entrypoints = {
+            logout = {
+              preferences = {
+                confirm = true;
+                customProgram = "${hyprshutdown} -t 'Logging out...'";
               };
-          };
-        };
-        theme = {
-          dark = {
-            icon_theme = "Adwaita";
-            name = "kanagawa";
+            };
+            power-off = {
+              preferences = {
+                confirm = true;
+                customProgram = "${hyprshutdown} -t 'Shutting down...' --post-cmd 'shutdown -P now'";
+              };
+            };
+            reboot = {
+              preferences = {
+                confirm = true;
+                customProgram = "${hyprshutdown} -t 'Restarting...' --post-cmd 'reboot'";
+              };
+            };
           };
         };
       };
+      theme = {
+        dark = {
+          icon_theme = "Adwaita";
+          name = "kanagawa";
+        };
+      };
+    };
   };
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/vicinae.nix
   # https://docs.vicinae.com/launcher-window
