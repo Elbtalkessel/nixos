5a0092aa3afc469f93ffc7c0fe0912b3d9144acf
Elbtalkessel <rtfsc@pm.me>
2026-06-18 15:23:06 +0200

    fix

--- home/modules/hypr/hyprland/workspace_rule.lua
+++ home/modules/hypr/hyprland/workspace_rule.lua
@@ -1,5 +1,5 @@
 -- https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 for i = 1, 10 do
-	local key = tostring(i % 10)
+	local key = i % 10
 	hl.workspace_rule({ workspace = key, persistent = true })
 end
