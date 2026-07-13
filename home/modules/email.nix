b1c963de39a2ddeb8d7f4e17b2ab3b47f3395e7a
Elbtalkessel <rtfsc@pm.me>
2026-07-13 22:09:26 +0200

    aerc conf

--- home/modules/email.nix
+++ home/modules/email.nix
@@ -9,10 +9,48 @@
     enable = true;
     extraPackages = with pkgs; [ gnome-keyring ];
   };
+
+  programs.aerc = {
+    enable = true;
+    extraConfig = {
+      filters = {
+        "text/plain" = "colorize";
+        "text/calendar" = "calendar";
+        "message/delivery-status" = "colorize";
+        "message/rfc822" = "colorize";
+        # Much cleaner HTML rendering
+        #"text/html" = "${lib.getExe pkgs.lynx} -dump -force_html -stdin | colorize";
+        "text/html" = "${lib.getExe pkgs.pandoc} -f html -t plain | colorize";
+      };
+      ui = {
+        threading-enabled = true;
+        threading-by-subject = true;
+        sort-thread-siblings = true;
+        sort = "-r date";
+        timestamp-format = "2006-01-02";
+        this-day-time-format = "15:04";
+        this-week-time-format = "Mon 15:04";
+        this-year-time-format = "Jan 02";
+        index-columns = "flags:4,name<20%,subject,date>12";
+        fuzzy-complete = true;
+        border-char-vertical = "│";
+        border-char-horizontal = "─";
+        styleset-name = "catppuccin-mocha";
+      };
+      viewer = {
+        pager = "less -FRX";
+      };
+      openers = {
+        "x-scheme-handler/http*" = "xdg-open {}";
+        "x-scheme-handler/mailto" = "xdg-open {}";
+      };
+    };
+  };
+
   home.packages = with pkgs; [
-    aerc
     libnotify
   ];
+
   systemd.user.services.goimapnotify = {
     Unit = {
       Description = "Protonmail Bridge Notifier";
@@ -44,7 +82,7 @@
               password: $PASSWORD
               boxes:
                 - mailbox: INBOX
-                  onNewMail: notify-send "%s" "You've got mail!"
+                  onNewMail: notify-send --app-name=aerc --icon=mail-unread "%s" "You've got mail!"
           EOF
         '';
 
