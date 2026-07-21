c60c4d846c9851adda36f5c935373e2ffd9f4235
Elbtalkessel <rtfsc@pm.me>
2026-07-21 12:21:53 +0200

    Tue 21 Jul 2026 12:21:53 CEST

--- home/modules/ide.nix
+++ home/modules/ide.nix
@@ -3,18 +3,22 @@
   home = {
     packages = with pkgs; [
       jetbrains-toolbox
-      # junie can't use nushell.
+
       # Patched JetBrains fsnotifier
       # reason: https://github.com/cachix/devenv/issues/1258
       # fix: https://github.com/NixOS/nixpkgs/pull/318358
       # related: https://github.com/JetBrains/intellij-community/pull/2171
-      # fsnotifier
+      fsnotifier
+
+      # junie requirement
+      unzip
     ];
   };
 
   xdg.configFile = {
     "ideavim/ideavimrc".source = ../config/ideavim/ideavimrc;
     # Sets filewatcher to patched one
-    #"idea/pycharm.properties".text = "idea.filewatcher.executable.path = ${pkgs.fsnotifier}/bin/fsnotifier";
+    "idea/pycharm.properties".text =
+      "idea.filewatcher.executable.path = ${pkgs.fsnotifier}/bin/fsnotifier";
   };
 }
