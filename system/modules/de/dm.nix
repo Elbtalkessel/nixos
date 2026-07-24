9952dd2648c4127b1225ff1ba075792b074ef7dd
Elbtalkessel <rtfsc@pm.me>
2026-07-24 10:38:43 +0200

    complete login screen config

--- system/modules/de/dm.nix
+++ system/modules/de/dm.nix
@@ -2,23 +2,29 @@
   services.displayManager = {
     enable = true;
     autoLogin = {
-      enable = true;
+      # TODO: enable it, but should unlock keyright by itself, if possible.
+      enable = false;
       user = config.my.username;
     };
     ly = {
       enable = true;
       x11Support = false;
-      # https://github.com/fairyglade/ly/blob/master/res/config.ini
+      # https://github.com/fairyglade/ly/blob/v1.4.1/res/config.ini
+      # - Some dur files (blackhole) require full_color to be true.
       settings = {
         animation = "doom";
+        full_color = true;
         animation_frame_delay = 40;
-        bigclock = "en";
-        hide_borders = true;
         doom_fire_height = 1;
         doom_fire_spread = 4;
-        corner_top_left = "shutdown,restart";
-        corner_bottom_left = null;
-        corner_bottom_right = null;
+        show_tty = true;
+        clock = "%F %T";
+        hide_version_string = true;
+        hide_borders = true;
+        brightness_down_cmd = null;
+        brightness_down_key = null;
+        brightness_up_cmd = null;
+        brightness_up_key = null;
       };
     };
   };
